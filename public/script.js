d32f2286b21dcc810e3151f61a2547d6e53eb96b
MJHC <magnus@bonusfamilien.dk>
2021-02-26 12:21:52 +0100

    created server

--- public/script.js
+++ public/script.js
@@ -5,9 +5,9 @@ const audioContainer = document.getElementById('audio-container')
 
 //connect to the pper server with "undefined" ID (generates uuid instead)
 const myPeer = new Peer(undefined, {
-  secure: true,
-  host: 'audp2p.herokuapp.com/',
-  port: '443'
+  host: 'audp2p.herokuapp.com',
+  port: '9000',
+  path: '/'
 })
 
 //userID and call storage
