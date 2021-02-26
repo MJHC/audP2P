695e19329e77653140ddde1b74368d39b06c691b
MJHC <magnus@bonusfamilien.dk>
2021-02-26 11:49:35 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+//npm i -g peer
+// peerjs --port 3001
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+io.on('connection', socket => {
+
+  //when a user is connected to the peer server do this
+  socket.on('voice', (userId) => {
+    socket.broadcast.emit('user-connected', userId)
+
+    //when a user disconnects do this
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user-disconnected', userId)
+  
+    })
+  })
+  
+})
+
+server.listen(3000)
