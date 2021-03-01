d6029bf8d5ba6189f6ea73add57d82e0f540ad56
MJHC <magnus@bonusfamilien.dk>
2021-03-01 09:05:25 +0100

    Only P2P server now

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
 const { PeerServer } = require('peer');
 const peerServer = PeerServer({ port: port, path: '/' });
 
-//npm i -g peer
-// peerjs --port 3001
-server.listen(port, () => {
-  console.log(`@ ${port}`);
-});
+console.log("listening @" + port);
+
